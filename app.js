ccd8060506fe42a15d038c7333a52716d0d54e32
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2025-01-16 20:14:32 -0700

    rename

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongodb = require('./db/connect');
+const mongodb = require('./database/connect');
 
 
 const port = process.env.PORT || 8080;
